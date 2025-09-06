7da1a937f8a59a4b0e54bab15af1f623e1b1584d
diego <diego.guerrero5dg@outlook.com>
2025-09-06 16:13:21 -0400

    Segundo commit java poo

--- Poo-java/Clase-padre/src/Main.java
+++ Poo-java/Clase-padre/src/Main.java
@@ -5,15 +5,19 @@ public class Main {
         //POO programación orientada a objetos Java:
         //---------------------------------------------------------------------------
         //Clases y objetos Java:
+        /*
+        * Definición de clase:
+        * */
 
         int edadPersona = 24;
         System.out.println("La edad de la persona es: " + edadPersona);
-        
+
         if (edadPersona >= 10) {
             System.out.println("La persona tiene más de 10 años");
         } else {
             System.out.println("La edad de la persona tiene menos de 10 años");
         }
+
 
 
     }
