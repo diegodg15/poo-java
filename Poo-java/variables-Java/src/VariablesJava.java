b6b041cc9fb43a7f4c3a8a0096ddae85e3f7f1f1
diego <diego.guerrero5dg@outlook.com>
2025-09-09 19:04:21 -0300

    Segundo commit java poo

--- Poo-java/variables-Java/src/VariablesJava.java
+++ Poo-java/variables-Java/src/VariablesJava.java
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class VariablesJava {
     public static void main(String[] args) {
-        //Cualquier código dentro del main()método se ejecutará.
+        //Cualquier código dentro del main()  método se ejecutar�.
 
         /**
          * Cada línea de código que se ejecuta en Java debe estar dentro de un
-         * class. Y el nombre de la clase siempre debe empezar con mayúscula
+         * class. Y el nombre de la clase siempre debe empezar con may�scula
          * inicial. En nuestro ejemplo, llamamos a la clase Main .
          *
          * Nota: Java distingue entre mayúsculas y minúsculas: "MyClass" y
@@ -15,27 +15,27 @@ public class VariablesJava {
 
         //----------------------------------------------------------------------
         //Tipos de datos primitivos:
-        //variables de tipo texto Tipo de carácter
+        //variables de tipo texto Tipo de car�cter
         String textoString = "texto";//almacena texto, cadena de texto .
 
-        char textocaracter = 't';//almacena caracteres individuales. Los valores se escriben entre comillas simples.
+        char textocaracter = 't';//almacena caracteres individuales. Los valores se describe entre comillas simples.
         //----------------------------------------------------------------------
 
         // 1 byte = 8 bits
         //variables de tipo números enteros Tipos enteros
         byte numeroByte = 123;//almacena números pequeños (8 bits).
 
-        short numeroShort = 12345;//almacenanúmeros más grandes que un byte (16 bits).
+        short numeroShort = 12345;//almacena números más grandes que un byte (16 bits).
 
-        int numeroInt = 2;//almacena numeros enteros.
+        int numeroInt = 2;//almacena números enteros.
 
         long numeroLong = 1234567891011121314L;//almacena números enteros muy grandes, se utiliza con el sufijo L.
         //----------------------------------------------------------------------
 
         //variables de tipo números con decimales o racionales Tipos de punto flotante
-        float numeroFloat = 12.7f;//almacena números de punto flotante, con decimales, de presición simple (32 bits). utiliza el sufijo f.
+        float numeroFloat = 12.7f;//almacena números de punto flotante, con decimales, de presici�n simple (32 bits). utiliza el sufijo f.
 
-        double numeroDouble = 2.43;//almacena númeroscon decimales, de presición doble (64 bits).
+        double numeroDouble = 2.43;//almacena números con decimales, de presici�n doble (64 bits).
         //puede almacenar un número mucho más grande y con una mayor cantidad de decimales que float.
         //----------------------------------------------------------------------
 
@@ -48,10 +48,10 @@ public class VariablesJava {
         //Tipos de declaración de variables:
         //FINAL: no puede cambiar su valor, osea es una variable constante.
         final double numeroPi = Math.PI;
-        //Si es una referencia a un objeto, no puedes cambiar el objeto, pero sí modificar sus propiedades internas:
+        //Si es una referencia a un objeto, no puedes cambiar el objeto, pero s� modificar sus propiedades internas:
         /**
          * final List<String> nombres = new ArrayList<>(); nombres.add("Ana");
-         * // ✅ permitido nombres = new ArrayList<>(); // ❌ no permitido
+         * // ? permitido nombres = new ArrayList<>(); // ? no permitido
          */
 
         //Un método final no puede ser sobrescrito por una subclase.
@@ -59,7 +59,7 @@ public class VariablesJava {
          * class Padre { public final void saludar() { System.out.println("Hola
          * desde Padre"); } }
          *
-         * class Hijo extends Padre { // public void saludar() { ... } // ❌
+         * class Hijo extends Padre { // public void saludar() { ... } // ?
          * Error: no se puede sobrescribir }
          */
         //Una clase final no puede ser extendida.
@@ -67,27 +67,27 @@ public class VariablesJava {
          * final class Utilidades { public static void imprimir() {
          * System.out.println("Método útil"); } }
          *
-         * // class MisUtilidades extends Utilidades {} // ❌ Error
+         * // class MisUtilidades extends Utilidades {} // ? Error
          */
         //Tipos:
         /**
-         * final normal	En la declaración o en el constructor	❌ No static final
-         * En la declaración o en bloque static	❌ No final en blanco	Solo en el
-         * constructor	❌ No
+         * final normal	En la declaraci�n o en el constructor	? No static final
+         * En la declaración o en bloque static	? No final en blanco	Solo en el
+         * constructor	? No
          */
         //Tipos de variables en java:
         /**
-         * Tipo de variable Local ¿Dónde se declara? Dentro de un método,
-         * constructor o bloque ¿Cuándo se crea? Al ejecutar ese bloque ¿Quién
+         * Tipo de variable Local Dónde se declara? Dentro de un método,
+         * constructor o bloque Cuándo se crea? Al ejecutar ese bloque Qui�n
          * puede acceder? Solo dentro de ese bloque
          *
-         * Tipo de variable De instancia ¿Dónde se declara? Dentro de la clase,
-         * fuera de métodos ¿Cuándo se crea? Al crear un objeto ¿Quién puede
+         * Tipo de variable De instancia Dónde se declara? Dentro de la clase,
+         * fuera de metodos �Cu�ndo se crea? Al crear un objeto �Qui�n puede
          * acceder? Cada objeto tiene su propia copia
          *
-         * Tipo de variable De clase (estática) ¿Dónde se declara? Dentro de la
-         * clase, con static ¿Cuándo se crea? Al cargar la clase en memoria
-         * ¿Quién puede acceder? Compartida por todos los objetos
+         * Tipo de variable De clase (est�tica) �D�nde se declara? Dentro de la
+         * clase, con static �Cu�ndo se crea? Al cargar la clase en memoria
+         * Qui�n puede acceder? Compartida por todos los objetos
          *
          */
     }
